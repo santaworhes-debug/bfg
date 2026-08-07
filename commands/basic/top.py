f500b8115860f6f0dcfea3d3ae31b0416d46a92e
santaworhes-debug <santaworhes@gmail.com>
2026-08-07 09:04:39 +0300

    Add files via upload

---
+++ commands/basic/top.py
@@ -0,0 +1,157 @@
+from aiogram import Dispatcher, types
+
+from assets.antispam import antispam_earning, new_earning, antispam
+from commands.db import url_name, top_db, get_name, top_clans_db
+from commands.clans.db import clan_full_info
+from filters.custom import TextIn, StartsWith
+from user import BFGuser, BFGconst
+from assets import keyboards as kb
+
+
+def get_num_user(num: list, user_position: int | None) -> str:
+	if user_position is not None and user_position <= 999:
+		emojis = ["0️⃣", "1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
+		return "".join(emojis[int(digit)] for digit in num)
+	return "➡️9️⃣9️⃣9️⃣"
+
+
+async def get_user_position(top_players: list, user_id) -> int | None:
+	for i, player in enumerate(top_players, start=1):
+		if i > 999:
+			break
+		if player[0] == user_id:
+			return i
+	return None
+
+
+def transform(summ: int) -> str:
+	summ = int(summ)
+	if summ > 100_000_000_000_000:
+		return "{:.2e}".format(float(summ))
+	return "{:,}".format(summ).replace(",", ".")
+
+
+async def get_username(tab: str, data: tuple) -> str:
+	if tab != "users":
+		return await get_name(data[0])
+	return data[1]
+
+
+async def handle_top(call, tab, st, index, top, top_emj) -> None:
+	user_id = call.from_user.id
+	userinfo, top_players = await top_db(user_id, st, tab)
+
+	user_position = await get_user_position(top_players, user_id)
+	url = await url_name(user_id)
+
+	top_message = f"{url}, топ 10 игроков бота по {top}:\n"
+	emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "1️⃣0️⃣"]
+
+	for i, player in enumerate(top_players[:10], start=1):
+		emj = emojis[i - 1]
+		value = transform(player[index])
+		name = await get_username(tab, player)
+		top_message += f"{emj} {name} — {value}{top_emj}\n"
+
+	top_message += f"—————————————————\n"
+
+	emoji = get_num_user(str(user_position), user_position)
+	value = transform(userinfo[index])
+	name = await get_username(tab, userinfo)
+	top_message += f"{emoji} {name} — {value}{top_emj}"
+
+	await call.message.edit_text(text=top_message, reply_markup=kb.top(user_id, st))
+
+
+async def handle_top_earning(call, tab, st, index, top, top_emj) -> None:
+	user_id = call.from_user.id
+	userinfo, top_players = await top_db(user_id, st, tab)
+	url = await url_name(user_id)
+
+	top_message = f"{url}, топ 10 игроков бота по {top}:\n"
+	emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "1️⃣0️⃣"]
+
+	for i, player in enumerate(top_players[:10], start=1):
+		emj = emojis[i - 1]
+		value = transform(player[index])
+		name = await get_username(tab, player)
+		top_message += f"{emj} {name} — {value}{top_emj}\n"
+
+	if userinfo:
+		user_position = await get_user_position(top_players, user_id)
+		emoji = get_num_user(str(user_position), user_position)
+		value = transform(userinfo[index])
+		name = await get_username(tab, userinfo)
+
+		top_message += f"—————————————————\n"
+		top_message += f"{emoji} {name} — {value}{top_emj}"
+
+	await call.message.edit_text(text=top_message, reply_markup=kb.top(user_id, st))
+
+
+@antispam
+async def top(message: types.Message, user: BFGuser):
+	msg = await message.answer(f"{user.url}, выберите ниже топ который хотите открыть", reply_markup=kb.top(user.user_id, "None"))
+	await new_earning(msg)
+
+
+@antispam_earning
+async def top_call(call: types.CallbackQuery, user: BFGuser):
+	tab = call.data.split("-")[1].split("|")[0]
+	type = call.data.split("|")[2]
+	
+	if tab == type:
+		return
+
+	if tab == "rating":
+		await handle_top(call, "users", "rating", 13, "рейтингу", "👑")
+	elif tab == "balance":
+		await handle_top(call, "users", "balance", 2, "балансу", "💲")
+	elif tab == "exp":
+		await handle_top(call, "users", "exp", 7, "опыту", "🏆")
+	elif tab == "yen":
+		await handle_top(call, "users", "yen", 22, "йенам", "💴")
+	elif tab == "case1":
+		await handle_top(call, "users", "case1", 9, "обычным кейсам", "📦")
+	elif tab == "case2":
+		await handle_top(call, "users", "case2", 10, "золотым кейсам", "🏵")
+	elif tab == "case3":
+		await handle_top(call, "users", "case3", 11, "рудным кейсам", "🏺")
+	elif tab == "case4":
+		await handle_top(call, "users", "case4", 12, "материальным кейсам", "🌌")
+	elif tab == "cards":
+		await handle_top_earning(call, "ferma", "cards", 3, "фермам", "🧰")
+	elif tab == "bsterritory":
+		await handle_top_earning(call, "business", "bsterritory", 4, "бизнесам", "🗄")
+
+
+@antispam
+async def top_clans(message: types.Message, user: BFGuser):
+	claninfo, top_clans = await top_clans_db(user.user_id)
+	user_position = None
+	
+	if claninfo:
+		d, _, _ = await clan_full_info(claninfo[1])
+		user_position = await get_user_position(top_clans, d[0])
+
+	top_message = f"{user.url}, топ 10 кланов:\n"
+	emojis = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"]
+
+	for i, clan in enumerate(top_clans[:10], start=1):
+		position_emoji = emojis[i - 1]
+		top_message += f"{position_emoji} {clan[2]} — {clan[12]}👑\n"
+
+	if user_position is not None:
+		top_message += f"—————————————————\n"
+		emoji = get_num_user(str(user_position), user_position)
+		top_message += f"{emoji} {d[2]} — {d[12]}👑"
+
+	top_message += f"\n\n{BFGconst.ads}"
+
+	await message.answer(top_message)
+
+
+def reg(dp: Dispatcher):
+	dp.message.register(top, TextIn("топ"))
+	dp.callback_query.register(top_call, StartsWith("top-"))
+	dp.message.register(top_clans, TextIn("клан топ"))
---
+++ commands/basic/transfer.py
@@ -0,0 +1,149 @@
+from decimal import Decimal
+
+from aiogram import types, Dispatcher
+
+from commands.db import getperevod, url_name
+from commands.admin.db import give_bcoins_db, give_money_db
+from filters.custom import TextIn, StartsWith
+from user import BFGuser, BFGconst
+from assets.transform import transform_int as tr
+from commands.admin.game_log import new_log
+from assets.antispam import antispam, admin_only
+import config as cfg
+
+
+def get_limit_cmd(status: int) -> int:
+    """Получить лимит на дневную передачу"""
+    if status == 1:
+        return 300_000_000_000_000
+    elif status == 2:
+        return 750_000_000_000_000
+    elif status == 3:
+        return 1_000_000_000_000_000
+    elif status == 4:
+        return 30_000_000_000_000_000
+    return 150_000_000_000_000  # Для статуса "игрок" (0)
+
+
+@antispam
+async def transfer_cmd(message: types.Message, user: BFGuser):
+    user_id = message.from_user.id
+    win, lose = BFGconst.emj()
+    limit = get_limit_cmd(user.status)
+
+    try:
+        reply_user_id = message.reply_to_message.from_user.id
+        url2 = await url_name(reply_user_id)
+    except:
+        await message.reply(f"{user.url}, чтобы передать деньги нужно ответить на сообщение пользователя {lose}")
+        return
+
+    if user_id == reply_user_id:
+        return
+
+    try:
+        summ = message.text.split()[1].replace("е", "e")
+        summ = int(float(summ))
+    except:
+        await message.reply(f"{user.url}, вы не ввели сумму которую хотите передать игроку {lose}")
+        return
+
+    limit = Decimal(str(limit)) + Decimal(int(user.perlimit))
+    d_per = Decimal(int(user.per)) + Decimal(str(summ))
+
+    if d_per > limit:
+        await message.reply(f"{user.url}, вы уже исчерпали свой дневной лимит передачи денег")
+        return
+
+    if summ > 0:
+        if int(user.balance) >= summ:
+            await message.answer(f"Вы передали {tr(summ)}$ игроку {url2} {win}")
+            await getperevod(summ, user_id, reply_user_id)
+            await new_log(f"#перевод\n{user_id}\nСумма: {tr(summ)}\nПередал: {reply_user_id}", "money_transfers")
+        else:
+            await message.reply(f"{user.url}, вы не можете передать больше чем у вас есть на балансе {lose}")
+
+    else:
+        await message.reply(f"{user.url}, вы не можете передать отрицательное число игроку {lose}")
+
+
+@antispam
+async def limit_cmd(message: types.Message, user: BFGuser):
+    limit = get_limit_cmd(user.status)
+
+    limit = int(limit) + int(user.perlimit)
+    per = int(user.per)
+    ost = limit - per
+
+    await message.reply(f"""{user.url}, здесь ваш лимит на сегодня: {tr(limit)}$
+💫 Вы уже передали: {tr(per)}$
+🚀 У вас осталось: {tr(ost)}$ для передачи!""")
+
+
+@antispam
+async def give_money(message: types.Message, user: BFGuser):
+    win, lose = BFGconst.emj()
+
+    if not (user.user_id in cfg.admin or user.status == 4):
+        await message.answer(
+            "👮‍♂️ Вы не являетесь администратором бота чтобы использовать данную команду.\n"
+            "Для покупки введи команду \"Донат\"")
+        return
+
+    try:
+        r_user_id = message.reply_to_message.from_user.id
+        r_url = await url_name(r_user_id)
+    except:
+        await message.answer(f"{user.url}, чтобы выдать деньги нужно ответить на сообщение пользователя {lose}")
+        return
+
+    try:
+        summ = message.text.split()[1].replace("е", "e")
+        summ = int(float(summ))
+    except:
+        await message.answer(f"{user.url}, вы не ввели сумму которую хотите выдать {lose}")
+        return
+
+    if user.user_id in cfg.admin:
+        await give_money_db(user.user_id, r_user_id, summ, "rab")
+        await message.answer(f"{user.url}, вы выдали {tr(summ)}$ пользователю {r_url}  {win}")
+    else:
+        res = await give_money_db(user.user_id, r_user_id, summ, "adm")
+        if res == "limit":
+            await message.answer(f"{user.url}, вы достигли лимита на выдачу денег  {lose}")
+            return
+
+        await message.answer(f"{user.url}, вы выдали {tr(summ)}$ пользователю {r_url}  {win}")
+
+    await new_log(f"#выдача\nИгрок {user.user_id}\nСумма: {tr(summ)}$\nИгроку {r_user_id}", "issuance_money")  # new log
+
+
+@admin_only()
+async def give_bcoins(message: types.Message):
+    user_id = message.from_user.id
+    win, lose = BFGconst.emj()
+
+    try:
+        r_user_id = message.reply_to_message.from_user.id
+        r_url = await url_name(user_id)
+    except:
+        await message.answer(f"Админ, чтобы выдать деньги нужно ответить на сообщение пользователя {lose}")
+        return
+
+    try:
+        summ = message.text.split()[1].replace("е", "e")
+        summ = int(float(summ))
+    except:
+        await message.answer(f"Админ, вы не ввели сумму которую хотите выдать {lose}")
+        return
+
+    await give_bcoins_db(r_user_id, summ)
+    await message.answer(f"Админ, вы выдали {tr(summ)}💳 пользователю {r_url}  {win}")
+    await new_log(f"#бкоин-выдача\nАдмин {user_id}\nСумма: {tr(summ)}$\nПользователю {r_user_id}", "issuance_bcoins")
+
+
+def reg(dp: Dispatcher):
+    dp.message.register(limit_cmd, TextIn("мой лимит"))
+    dp.message.register(transfer_cmd, StartsWith("дать"))
+    dp.message.register(give_money, StartsWith("выдать"))
+    dp.message.register(give_bcoins, StartsWith("бдать"))
